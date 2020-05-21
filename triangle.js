fc4b28134ae5a9706b72bd850fded9b55be8e131
glitchgirl <londonspleen@gmail.com>
2020-05-21 12:12:10 -0400

    file structure corrections

--- triangle.js
+++ triangle.js
@@ -1,6 +1,6 @@
 // given base and height calculates area of a triangle
 // things to add would be to make sure these are numbers, and if its a valid triangle 
-module.exports = function triangleArea(base,height){
+module.exports = function (base,height){
     return ((base *height)/2);
 };
 
